1a5176edc9d67c89779f292c48f29d64f32d34ac
ehsan <ehsansaraee6570@yahoo.com>
2021-10-30 23:37:59 +0330

    Update the Store States & Connect Redux to App

--- src/Redux/Features/reservationSlice.ts
+++ src/Redux/Features/reservationSlice.ts
@@ -1,13 +1,23 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-const initialState = {
+export interface Reservation {
+   value: string[];
+}
+
+const initialState: Reservation = {
    value: [],
 };
 
 const reservationSlice = createSlice({
    name: 'reservation',
    initialState,
-   reducers: {},
+   reducers: {
+      addReservation: (state, action: PayloadAction<string>) => {
+         state.value.push(action.payload);
+      },
+   },
 });
+
+export const { addReservation } = reservationSlice.actions;
 
 export default reservationSlice.reducer;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import { store } from './Redux/App/store';
 
 const rootElement = document.getElementById('root');
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+   <Provider store={store}>
+      <App />
+   </Provider>,
+   rootElement
+);
